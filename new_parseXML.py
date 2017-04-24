2a3e9ca1d085731ab70f5d1b9ca982cc54ebf755
cheng <cheng@dx.com>
2017-04-24 10:23:18 +0800

    完善解析XML方法与写入方法

--- new_parseXML.py
+++ new_parseXML.py
@@ -6,7 +6,8 @@ import sys
 
 
 #使用xml.dom.minidom解析XML时，要获取各节点的属性值。
-#使用树的方式进行解析。一层一层往下解析
+#所有的XML解析，其实就是关注节点与属性两个东西就可以了
+#因为XML是树结构，所要一层一层往下解析
 
 try:
     xml_dom = xml.dom.minidom.parse(sys.argv[1])
@@ -28,3 +29,33 @@ for moive in moive_ele_lst:
     #获取type的属性值
     print moive_type_ele.childNodes[0].data
     break
+
+
+##Type two
+from xml.dom.minidom import parse
+from xml.dom.minidom import Element
+
+
+#定义过滤子节点带有'\n'函数
+def filter_dom(doms):
+    doms = [ node for node in doms if not hasattr(node,'data')]
+    return doms
+
+xml = parse('log4j.xml')
+#获取根节点
+root_ele = xml.documentElement
+#获取当前节点下的所有子节点。这里是第二层节点
+sec_ele = filter_dom(root_ele.childNodes)
+#获取当前节点的指定属性值
+root_ele.getAttribute('xmlns:log4j')
+#获取当前节点所有的属性值与Value,返回列表数据
+root_ele.arributes.items()
+
+
+#写入示例
+#写入使用DOM对象的appendChild()方法，它传入xml.dom.minidom.Element的实例对象
+cheng_ele = Element('cheng') 
+cheng_ele.setAttribute('name','cheng')
+root_ele.appendChild(cheng_ele)
+with open('new.xml','w') as f:
+    f.write(root_ele.toprettyxml(encoding='utf8'))
